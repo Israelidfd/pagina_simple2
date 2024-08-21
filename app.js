06a5cd35b5b2370cb8a94a376c1630aa0695633d
israel <116818850+Israelidfd@users.noreply.github.com>
2024-08-21 03:35:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express= require("express")
+
+const app = express()
+
+app.get("/index",(req,res)=>{
+
+    app.set("view engine", "ejs")
+
+    res.render("index")
+})
+
+app.get("/footer",(req,res)=>{
+
+    app.set("view engine", "ejs")
+
+    res.render("footer")
+})
+
+
+app.listen(3000, (req,res)=> {
+    console.log("Corriendo en el puerto 3000")
+})
